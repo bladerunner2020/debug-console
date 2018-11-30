2152f4144010cf13d15269fcdc40bd01b2b9ee8c
bladerunner2020 <pivovarov@gmail.com>
2018-11-30 14:31:23 +0300

    Добавлена возможность получать нотификацию при изменении настроек

--- test/app.js
+++ test/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/*global IR, DebugConsole*/
 
 var debugConsole = null;
 
@@ -5,7 +7,6 @@ function kill() {
     if (!debugConsole) return;
 
     debugConsole.kill();
-
     debugConsole = null;
 }
 
@@ -19,44 +20,57 @@ function initConsole() {
         noConsoleLog: false,
         // defaultPage: 'Main',
         debugPage: 'DebugPopup'});
+    
+    debugConsole.on('settings', function(arg1, arg2, arg3) {
+        _Log('Settings are changed. ' + arg1 + ': ' + arg2 + ' = ' + arg3);
+    });
 
-// Устанавливаем начальные настройки отладочной консоли
+    // Устанавливаем начальные настройки отладочной консоли
     debugConsole
         .setEventFilter('debug', true)  // Не показывать события debug
         .showField('source', false)     // Не показывать поле source
         .setFieldSizes({source : 15});
 }
 
-
 // Функции для вывода отладочной информации вместо IR.Log
 // Если у debugConsole не стоит noConsoleLog, то вывод дублируется в IR.Log
 function _Debug(message) {
-    if (!debugConsole) return;
+    if (!debugConsole) {
+        return;
+    }
 
     debugConsole.log({event: 'debug', message: message, source: 'SCRIPT', timestamp : new Date()});
 }
 
 function _Log(message) {
-    if (!debugConsole) return;
+    if (!debugConsole) {
+        return;
+    }
 
     debugConsole.log(message);
 }
 
 function _Error(message) {
-    if (!debugConsole) return;
+    if (!debugConsole) {
+        return;
+    }
 
     debugConsole.log('ERROR: ' + message);
 }
 
 function _Warning(message) {
-    if (!debugConsole) return;
+    if (!debugConsole) {
+        return;
+    }
 
     debugConsole.log('WARNING: ' + message);
 }
 
 // Функция для показа отладочной панели 
 function showDebugConsole() {
-    if (!debugConsole) return;
+    if (!debugConsole) {
+        return;
+    }
 
     debugConsole.showConsole();
 }
@@ -72,21 +86,18 @@ IR.SetInterval(1000, function () {
 
     var count2 = debugConsole ? debugConsole.getMessageCount(0, true) : count;
 
-
     switch (index) {
-        case 1:
-            _Debug('count = ' + count2);
-            break;
-        case 2:
-            _Log('count = ' + count);
-            break;
-        case 3:
-            _Error('count = ' + count);
-            break;
-        case 4:
-            _Warning('count = ' + count);
+    case 1:
+        _Debug('count = ' + count2);
+        break;
+    case 2:
+        _Log('count = ' + count);
+        break;
+    case 3:
+        _Error('count = ' + count);
+        break;
+    case 4:
+        _Warning('count = ' + count);
     }
 });
-
-
 
